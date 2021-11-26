39b1360b31f68dfd7b36b5f6e76c21aab54ebbbe
Priyankajishnu <93514254+Priyankajishnu@users.noreply.github.com>
2021-11-26 15:13:02 +0530

    studyupdate

---
+++ api.js
@@ -0,0 +1,45 @@
+
+function ajax(){
+    var xhttp=new XMLHttpRequest();
+    xhttp.onreadystatechange=function(){
+        if(this.readyState==4&&this.status==200){
+            var response=JSON.parse(this.responseText);
+            // var counter=0;
+            var output="<table><th></th><th>TITLE</th></table>";
+            for(var i=0;i<response.length;i++){
+                if(response[i].completed==true){
+                    output+=("<tr><td>"+'<input type="checkbox"  id="checkbox" checked=true disabled  >'+"</td><td>"+response[i].title+"</td></tr>");
+                    
+                }
+                else{
+                    output+=("<tr><td>"+'<input type="checkbox" id="checkbox" onclick=updateList()>'+"</td><td>"+response[i].title+"</td><tr>")
+                }
+                // output+=("<tr><td>"+"<input id=remember type=checkbox unchecked>"+"</td><td>"+response[i].title+"</td></tr>");
+            }
+            document.getElementById("myTable").innerHTML=output;
+        }
+        
+    }
+    xhttp.open("GET","https://jsonplaceholder.typicode.com/todos",true);
+    xhttp.send();
+}
+ajax();
+
+var counter;
+function updateList(){
+        
+        var myCheck = document.getElementById("checkbox");
+        
+        var compltedTasks = document.querySelectorAll('input[type="checkbox"]:disabled').length;
+        
+        var checkedTasks = document.querySelectorAll('input[type="checkbox"]:checked').length;
+         counter=checkedTasks-compltedTasks;    
+       if (counter==5){
+        alert("Congrats!! 5 Tasks have been Successfully Completed");
+       }
+}
+
+
+
+
+
---
+++ form.js
@@ -0,0 +1,29 @@
+let uname=document.getElementById("uname");
+let pwd=document.getElementById("pwd");
+let loginform=document.getElementById("loginform");
+let err1=document.getElementById("err1");
+let err2=document.getElementById("err2");
+function validate(){
+    let uname=document.getElementById("uname");
+    let pwd=document.getElementById("pwd");
+    // let err1=document.getElementById("err1");
+    let err2=document.getElementById("err2");    
+    if(uname.value=="admin" && pwd.value=="12345"){
+        err2.innerHTML="Success!!";
+        login();
+        // err1.innerHTML="Success!!";
+       return true;
+    }
+    else{
+       
+        // err1.innerHTML="Invalid Username";
+        // err1.style.color="red";
+        err2.innerHTML="Invalid Username or Password";
+        err2.style.color="red";
+        return false;
+        
+    }
+}
+function login(){
+    document.getElementById("loginform").setAttribute("action","./list.html");
+}
